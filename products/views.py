d020e27d3874d575c8913f4bdad81cba2670f646
Eidocode <eidocode@orange.fr>
2021-01-07 23:31:20 +0100

    - Added Bootstrap template - Added index.html

--- products/views.py
+++ products/views.py
@@ -1,7 +1,5 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def index(request):
-    message = "Hello World !!!"
-    return HttpResponse(message)
+    return render(request, 'products/index.html')
--- purbeurre_project/settings.py
+++ purbeurre_project/settings.py
@@ -34,7 +34,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    #'main_app.apps.MainAppConfig',
+    'products.apps.ProductsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
