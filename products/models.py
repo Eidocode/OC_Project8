4c4e70bcd770970de4015969ead57df9ae3e3117
Eidocode <eidocode@orange.fr>
2021-01-14 23:32:52 +0100

    Added project models.py

--- products/models.py
+++ products/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    brand = models.CharField(max_length=200)
+    description = models.TextField()
+    score = models.CharField(max_length=1)
+    barcode = models.CharField(max_length=50)
+    url_img= models.URLField()
+    categories = models.ManyToManyField(Category, related_name='products')
+
+
+class User(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=100)
+    added_date = models.DateTimeField(auto_now_add=True)
+
+
+class Favorite(models.Model):
+    added_date = models.DateTimeField(auto_now_add=True)
+    products = models.ForeignKey(Product, on_delete=models.CASCADE)
+    users = models.ForeignKey(User, on_delete=models.CASCADE)
