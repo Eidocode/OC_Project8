c8c1e54bc317b6d1fdd59877e9104239e00defbe
Eidocode <eidocode@orange.fr>
2021-01-30 10:48:20 +0100

    Added icons in place of words in Navbar

--- purbeurre_project/settings.py
+++ purbeurre_project/settings.py
@@ -82,27 +82,27 @@ WSGI_APPLICATION = 'purbeurre_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'pur_beurre',
-#         'USER': 'admdjango',
-#         'PASSWORD': 'django',
-#         'HOST': '192.168.1.59',
-#         'PORT': '5432'
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'pur_beurre',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': '10.0.64.227',
+        'USER': 'admdjango',
+        'PASSWORD': 'django',
+        'HOST': '192.168.1.59',
         'PORT': '5432'
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'pur_beurre',
+#         'USER': 'postgres',
+#         'PASSWORD': 'postgres',
+#         'HOST': '10.0.64.227',
+#         'PORT': '5432'
+#     }
+# }
 
 
 # Password validation
