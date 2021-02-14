8eed8f1b066f24090298378391e70e447104e83d
David Bouzerar <eidocode@orange.fr>
2021-02-14 19:17:59 +0100

    - Finished fill_db_categories PEP8 & documentation - Finished fill_db_products PEP8 & documentation - Added Requirements.txt

--- products/management/commands/fill_db_categories.py
+++ products/management/commands/fill_db_categories.py
@@ -7,80 +7,170 @@ from random import randrange
 from products.models import Category
 
 
-URL = 'https://fr.openfoodfacts.org/'  # /categorie/[name_cat].json
+URL = 'https://fr.openfoodfacts.org/'  # + /categorie/[name_cat].json
 
-MIN_PRODUCTS_TO_FILTER = 100
+MIN_PRODUCTS_TO_FILTER = 100  # Minimum number of products in a category
 
 
 class Command(BaseCommand):
+    """
+    Class used to add a new parameter fill_db_categories to manage.py
+
+    ...
+
+    Methods
+    -------
+    add_arguments(parser)
+        Adds int argument for command line
+
+    handle()
+        Contains the method called when executed the command line
+        (set_categories_to_db) with the specified int argument
+
+    _get_categories(min_nb_prod)
+        Method used to get OpenFoodFacts categories from and returns those
+        filtered by min_nb_prod
+
+    _get_random_categories(nb_cat)
+        Returns some random categories contained in _get_categories defined by
+        int nb_cat
+
+    _compare_with_db(nb_cat)
+        Compares the data in the database with data received from openfoodfacts
+        to avoid adding duplicates. Returns a clean list of categories
+
+    _set_categories_to_db(nb_cat)
+        Used to set new categories to database
+
+    """
+
     help = 'Adds categories and products in pur_beurre database'
 
     def add_arguments(self, parser):
-        parser.add_argument('nb_catg', type=int, help='Indicates the number of categories to be created')
+        """Adds int argument for command line"""
+
+        parser.add_argument(
+            'nb_catg',
+            type=int,
+            nargs='?',
+            default=2,
+            help='Indicates the number of categories to be created',
+        )
 
     def handle(self, *args, **options):
-        nb_cats = options['nb_catg']
+        """Contains the method called when executed the command line
+        (set_categories_to_db) with the specified int argument"""
+
+        nb_cats = options['nb_catg']  # Gets nb_catg argument
         self.stdout.write("Processing for %s categories..." % nb_cats)
-        self.set_categories_to_db(nb_cats)
+        self._set_categories_to_db(nb_cats)  # set_categories_to_db method
 
     def _get_categories(self, min_nb_prod):
-        self.stdout.write("Getting categories containing at least %s products..." % min_nb_prod)
+        """Method used to get OpenFoodFacts categories from and returns those
+        filtered by min_nb_prod"""
+
+        self.stdout.write(
+            "Getting categories containing at least %s products" % min_nb_prod)
         list_cat = []
         list_cat_filtered = []
 
-        json_cat = requests.get(URL + 'categories.json').json()
+        # Getting all categories from openfoodfacts
+        json_cat = requests.get(
+            URL + 'categories.json',
+            headers={
+                'content-type': 'application/json',
+                'User-Agent': 'python-requests - PurbeurreApp'
+                }
+        ).json()
         list_cat = json_cat.get('tags')
 
         for category in list_cat:
-            if (category['products'] >= min_nb_prod and category['id'].startswith('fr')):
-                json_category = requests.get(category['url'] + '.json').json()
+            # Filter categories by 'id' starting with fr and containing at
+            # least min_nb_prod products
+            if (category['products'] >= min_nb_prod and
+                    category['id'].startswith('fr')):
+                json_category = requests.get(
+                    category['url'] + '.json',
+                    headers={
+                        'content-type': 'application/json',
+                        'User-Agent': 'python-requests - PurbeurreApp',
+                        }
+                    ).json()
                 nb_products = int(json_category.get('count'))
                 if nb_products is not None:
                     if nb_products >= min_nb_prod:
                         list_cat_filtered.append(category)
 
-        print("Categories >= {} product(s) : {}".format(min_nb_prod, str(len(list_cat_filtered))))
+        print("Categories >= {} product(s) : {}".format(
+            min_nb_prod,
+            str(len(list_cat_filtered))
+        ))
 
         return list_cat_filtered
 
     def _get_random_categories(self, nb_cat):
+        """Returns some random categories contained in _get_categories defined by
+        int nb_cat"""
+
+        # Gets Openfoodfacts categories
         lst = self._get_categories(MIN_PRODUCTS_TO_FILTER)
+
+        if nb_cat > len(lst):
+            # Checks if nb_cat > lst length
+            nb_cat = len(lst)
+            print('Number of categories is less than the parameter...')
+
         self.stdout.write("Getting %s random categories..." % nb_cat)
 
         categories = []
         for i in range(nb_cat):
+            # Random selection of categories
             index = randrange(0, len(lst))
             categories.append(lst.pop(index))
 
         return categories
 
     def _compare_with_db(self, nb_cat):
-        categories_in_db = Category.objects.all()
+        """Compares the data in the database with data received from openfoodfacts
+        to avoid adding duplicates. Returns a clean list of categories"""
+
+        categories_in_db = Category.objects.all()  # Gets categories from db
         json_ids_in_db = []
         for category in categories_in_db:
+            # Gets json_id from each category
             json_ids_in_db.append(category.json_id)
 
         new_categories = self._get_random_categories(nb_cat)
-        self.stdout.write("Compares the selection with existing data stored in the database...")
+        self.stdout.write(
+            "Compares selection with existing data stored in the database...")
 
         categories_list = []
         for category in new_categories:
+            # Checks if new category id exists in db
             if category['id'] not in json_ids_in_db:
                 categories_list.append(category)
             else:
-                print("{} with json_id : {} already in database...".format(category['name'], category['id']))
+                print("{} with json_id : {} already in database...".format(
+                    category['name'],
+                    category['id'])
+                )
 
         return categories_list
 
-    def set_categories_to_db(self, nb_cat):
-        categories = self._compare_with_db(nb_cat)
-        self.stdout.write("Adding %s categories in the database..." % len(categories))
+    def _set_categories_to_db(self, nb_cat):
+        """Used to set new categories to database"""
+        categories = self._compare_with_db(nb_cat)  # Gets new categories
+        self.stdout.write(
+            "Adding %s categories in the database..." % len(categories))
 
+        # Adds new categories in db
         for category in categories:
             new_category = Category(
                 name=category['name'],
                 json_id=category['id'],
                 url=category['url']
             )
-            print('Added new category : {} to database'.format(category['name']))
-            new_category.save()
+            print('Added new category : {} to database'.format(
+                category['name'])
+            )
+            new_category.save()  # Save category to db
--- products/management/commands/fill_db_products.py
+++ products/management/commands/fill_db_products.py
@@ -1,45 +1,87 @@
 import requests
 
 from django.core.management.base import BaseCommand
-from django.utils import timezone
-
 from random import randrange
 from math import ceil
 
 from products.models import Category, Product
 
 
-URL = 'https://fr.openfoodfacts.org/'  # /categorie/[name_cat].json
-
-NB_CATEGORIES_TO_GET = 20
-MIN_PRODUCTS_TO_FILTER = 100
-PRODUCTS_PER_PAGE = 20
-PRODUCTS_PER_CATG = 20
-
-NB_TRY = 3
+PRODUCTS_PER_PAGE = 20  # Products per page of a category in openfoodfacts
 
 
 class Command(BaseCommand):
+    """
+    Class used to add a new parameter fill_db_products to manage.py
+
+    ...
+
+    Methods
+    -------
+    add_arguments(parser)
+        Adds int argument for command line
+
+    handle()
+        Contains the method called when executed the command line
+        (_get_random_products) with the specified int argument
+
+    _get_categories_url()
+        Gets from database all categories url
+
+    _get_categories_jsonid()
+        Gets from database categories id & jsonid
+
+    _get_products_from_url(url)
+        Gets all products from category openfoodfacts url
+
+    _get_random_products(nb_prod)
+        Returns some random products defined by int nb_prod
+
+    _test_product_keys(product)
+        Tests each key in product to define viability
+
+    _get_products_db_barcode()
+        Gets all products barcodes in database
+    """
+
     help = 'Adds categories and products in pur_beurre database'
 
     def add_arguments(self, parser):
-        parser.add_argument('nb_prod', type=int, help='Indicates the number of products by category to be created')
+        """Adds int argument for command line"""
+
+        parser.add_argument(
+            'nb_prod',
+            type=int,
+            nargs='?',
+            default=5,
+            help='Indicates the number of products by category to be created'
+        )
 
     def handle(self, *args, **options):
-        nb_prod = options['nb_prod']
-        self.stdout.write("Processing for %s products by category..." % nb_prod)
-        self._get_random_products(nb_prod)
+        """Contains the method called when executed the command line
+        (set_categories_to_db) with the specified int argument"""
+
+        nb_prod = options['nb_prod']  # Gets nb_prod argument
+        self.stdout.write(
+            "Processing for %s products by category..." % nb_prod
+        )
+        self._get_random_products(nb_prod)  # _get_random_products method
 
     def _get_categories_url(self):
-        categories_in_db = Category.objects.all()
+        """Gets from database all categories url"""
+
+        categories_in_db = Category.objects.all()  # Gets categories from db
         categories_url = []
         for category in categories_in_db:
+            # Gets category url
             categories_url.append(category.url)
 
         return categories_url
-    
+
     def _get_categories_jsonid(self):
-        categories_in_db = Category.objects.all()
+        """Gets from database categories id & jsonid"""
+
+        categories_in_db = Category.objects.all()  # Gets categories from db
         categories_jsonid = []
         for category in categories_in_db:
             data = {
@@ -51,24 +93,45 @@ class Command(BaseCommand):
         return categories_jsonid
 
     def _get_products_from_url(self, url):
+        """Gets all products from category openfoodfacts url"""
+
+        # Attempt to connect to the specified url
         try:
-            url_json = requests.get(url + '.json')
+            url_json = requests.get(
+                url + '.json',
+                headers={
+                    'content-type': 'application/json',
+                    'User-Agent': 'python-requests - PurbeurreApp',
+                    }
+                )
             print('Connecting to {}...'.format(url))
         except requests.exceptions.ConnectionError:
             print('Unable to connect to {}, skip the category...'.format(url))
+            return
+        except requests.exceptions.ConnectionError:
+            print('An error has occured with {}...'.format(url))
             return
 
         self.stdout.write("Getting products from " + url)
         json_data = url_json.json()
 
-        nb_products = int(json_data.get('count'))
+        nb_products = int(json_data.get('count'))  # Nb products in category
+
+        # Nb pages defined by PRODUCTS_PER_PAGE
         nb_pages = ceil(nb_products / PRODUCTS_PER_PAGE)
 
         lst_all_prods = []
         i = 1
+        # Gets products from all pages
         for i in range(nb_pages):
             page_url = "{}/{}.json".format(url, str(i))
-            json_url_products = requests.get(page_url).json()
+            json_url_products = requests.get(
+                page_url,
+                headers={
+                    'content-type': 'application/json',
+                    'User-Agent': 'python-requests - PurbeurreApp',
+                    }
+                ).json()
             lst_products = json_url_products.get('products')
 
             for product in lst_products:
@@ -77,6 +140,9 @@ class Command(BaseCommand):
         return lst_all_prods
 
     def _get_random_products(self, nb_prod):
+        """Returns some random products defined by int nb_prod"""
+
+        # Gets Categories jsonid, url & products barcodes
         categories_db_jsonid = self._get_categories_jsonid()
         products_db_barcode = self._get_products_db_barcode()
         categories_url = self._get_categories_url()
@@ -84,6 +150,7 @@ class Command(BaseCommand):
         list_rnd_products = []
         count = 0
         for url in categories_url:
+            # Gets products from categories url
             products_in_category = self._get_products_from_url(url)
 
             if products_in_category is None:
@@ -94,10 +161,13 @@ class Command(BaseCommand):
 
             if nb_prod > len(products_in_category):
                 nb_prod = len(products_in_category)
-                print("Only {} products available in this category...".format(nb_prod))
+                print(
+                    "{} products available in this category...".format(nb_prod)
+                )
 
             rnd_prod_catg = []
             for i in range(nb_prod):
+                # Gets some products randomly
                 index = randrange(0, len(products_in_category))
                 rnd_prod_catg.append(products_in_category.pop(index))
                 i += 1
@@ -105,7 +175,8 @@ class Command(BaseCommand):
             list_rnd_products.append(rnd_prod_catg)
 
             for product in rnd_prod_catg:
-                data = self.test_product_keys(product)
+                # Test products data
+                data = self._test_product_keys(product)
                 if data is not None:
                     prod = Product(
                         name=data['name'],
@@ -119,16 +190,26 @@ class Command(BaseCommand):
                         url_img_nutrition=data['url_img_nutrition'],
                     )
 
+                    # Checks if product barcode exists in db
                     if data['barcode'] not in products_db_barcode:
                         prod.save()
                     else:
-                        print('{} with barcode : {} already in database'.format(data['name'], data['barcode']))
+                        print(
+                            '{} with barcode : {} already in database'.format(
+                                data['name'],
+                                data['barcode'])
+                            )
                         continue
 
                     for catg in categories_db_jsonid:
+                        # Linking (if any) in database between new products
+                        # and existing categories
                         if catg['json_id'] in product['categories_tags']:
                             prod.categories.add(catg['id'])
-                            print('Add relation between {} and category id {}'.format(data['name'], catg['id']))
+                            print('Links {} with category id {}'.format(
+                                data['name'],
+                                catg['id'])
+                            )
 
                     count += 1
             print('')
@@ -136,8 +217,9 @@ class Command(BaseCommand):
         print(str(count) + ' viables products.')
 
         return list_rnd_products
-        
-    def test_product_keys(self, product):
+
+    def _test_product_keys(self, product):
+        """Tests each key in product to define viability"""
 
         data = None
         try:
@@ -155,16 +237,17 @@ class Command(BaseCommand):
             }
         except KeyError:
             print('This product is not viable')
-                    
+
         return data
-    
+
     def _get_products_db_barcode(self):
-        products = Product.objects.all()
+        """Gets all products barcodes in database"""
+
+        products = Product.objects.all()  # Gets all products from database
         products_barcode = []
 
         for product in products:
+            # Gets product barcode
             products_barcode.append(product.barcode)
-        
-        return products_barcode
 
-        
+        return products_barcode
